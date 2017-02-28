c16b9796d5f7dce03c55cc29a489fb6a7d08c7e5
1600 <nanifold@gmail.com>
2017-02-28 11:04:48 +0800

    1

--- geventTest.py
+++ geventTest.py
@@ -2,13 +2,20 @@ import gevent
 
 def foo():
     print "running in foo"
-    #gevent.sleep(0)
+    gevent.sleep(0)
     print "context switch to foo again"
 
 def bar():
     print 'switching to bar'
     gevent.sleep(0)
-    print 'implicit context switch'
+    print 'back to bar....'
 
-gevent.joinall([gevent.spawn(foo),gevent.spawn(bar)])
+def koo():
+    print 'switching to koo'
+    gevent.sleep(0)
+    print 'back to koo again!'
+
+
+gevent.joinall([gevent.spawn(foo),gevent.spawn(bar),gevent.spawn(koo)])
+
 
